b41a5e2b9e948a40c86308544dea21356bd9e913
Aysha Nashra <ayshasblooms@gmail.com>
2025-10-31 22:23:37 +0530

    product page

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const image = document.getElementById("product-image");
+const title = document.getElementById("product-title");
+const price = document.getElementById("product-price");
+const desc = document.getElementById("product-description");
+const btn = document.getElementById("addtocart");
+
+fetch("https://fakestoreapi.com/products/2")
+  .then(response => response.json())
+  .then(product => {
+    image.src = product.image;
+    title.textContent = product.title;
+    price.textContent = `$${product.price}`;
+    desc.textContent = product.description;
+  })
+  .catch(error => console.log("Error fetching product:", error));
+
+btn.addEventListener("click", () => {
+  alert("✅ Added to cart!");
+});
